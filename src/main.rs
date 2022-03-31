edaf746a1554082a8b43efb70c83b13dc5a17a42
mahir31 <mahirajmal@live.com>
2022-03-31 22:42:48 +0100

    removed messagebuilder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use serenity::async_trait;
-use serenity::utils::MessageBuilder;
 use serenity::client::{Client, Context, EventHandler};
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
@@ -21,28 +20,6 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     
-    async fn message(&self, context: Context, msg: Message) {
-        if msg.content == "$ping" {
-            let channel = match msg.channel_id.to_channel(&context).await {
-                Ok(channel) => channel,
-                Err(why) => {
-                    println!("Error getting channel: {:?}", why);
-                    return;
-                },
-            };
-            let response = MessageBuilder::new()
-                .push("User ")
-                .push_bold_safe(&msg.author.name)
-                .push(" used the 'ping' command in the ")
-                .mention(&channel)
-                .push(" channel")
-                .build();
-            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{:?} is connected!", ready.user.name);
     }
@@ -70,7 +47,7 @@ async fn main() {
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
+    msg.reply(ctx, "yes!").await?;
 
     Ok(())
 }
