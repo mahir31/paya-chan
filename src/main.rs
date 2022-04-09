17f092b72d944bb8cf608b159ce3f9b4d2d59eb3
mahir31 <mahirajmal@live.com>
2022-04-09 18:13:30 +0100

    put commands in folders and stuff

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,83 @@
-use std::env;
+mod commands;
+
+use std::{collections::HashSet, env, sync::Arc};
+use commands::meta::*;
 use serenity::{
     async_trait,
-    client::{
-        Client,
-        Context,
-        EventHandler,
-    },
-    model::{
-        channel::Message,
-        gateway::Ready,
-    },
-    framework::standard::{
-        StandardFramework,
-        CommandResult,
-        macros::{
-            command,
-            group,
-        }
-    }
+    client::bridge::gateway::ShardManager,
+    framework::{standard::macros::group, StandardFramework},
+    http::Http,
+    model::{event::ResumedEvent, gateway::Ready},
+    prelude::*,
 };
+use tracing::{error, info};
 
-#[group]
-#[commands(ping)]
-struct General;
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    
+
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{:?} is connected!", ready.user.name);
     }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
 }
+
+#[group]
+#[commands(ping)]
+struct General;
 
 #[tokio::main]
 async fn main() {
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("$")) // set bot prefix
-        .group(&GENERAL_GROUP);
+
+    dotenv::dotenv().expect("Failed to load .env file");
+    tracing_subscriber::fmt::init();
+    let token = env::var("DISCORD_TOKEN").expect("Failed to load token");
+    let http = Http::new_with_token(&token);
+
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = 
+        StandardFramework::new().configure(|c| c.owners(owners).prefix("$")).group(&GENERAL_GROUP);
 
     // login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let mut client = Client::builder(token)
+    let mut client = Client::builder(&token)
         .event_handler(Handler)
         .framework(framework)
         .await
         .expect("Error creating client");
 
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start_shards(1).await {
-        println!("An error occurred while running the client {:?}", why);
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
     }
-}
 
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "yes!").await?;
+    let shard_manager = client.shard_manager.clone();
 
-    Ok(())
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        error!("Client error {:?}", why);
+    }
 }
