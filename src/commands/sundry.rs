31daae8cbbc79e399de87154557fc2ea6847899f
mahir31 <mahirajmal@live.com>
2022-04-12 19:50:41 +0100

    lines from file function but i don't know how useful it is just using it to learn lines from bufreader i guess

--- src/commands/sundry.rs
+++ src/commands/sundry.rs
@@ -1,7 +1,23 @@
-use std::fs;
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("unable to read file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|s| s.expect("could not parse line"))
+        .collect()
+}
 
 fn main() {
-    let data = fs::read_to_string("../../fortunes/fortunes").expect("unable to read file");
-    let data: Vec<String> = data.split("%").map(|s| s.to_string()).collect();
-    dbg!(&data);
+    // let data = fs::read_to_string("../../fortunes/fortunes").expect("unable to read file");
+    // let data: Vec<String> = data.split("%").map(|s| s.to_string()).collect();
+    // dbg!(&data);
+    let lines = lines_from_file("../../fortunes/fortunes");
+    for line in lines {
+        println!("{:?}", line);
+    }
 }
