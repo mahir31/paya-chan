94e175a08bb2e81a8244b032e3f65ceaa31a88b0
mahir31 <mahirajmal@live.com>
2022-04-14 16:17:44 +0100

    read file into String rather than string slice - need to go back and finish learning Rust before i continue on with this

--- src/commands/sundry.rs
+++ src/commands/sundry.rs
@@ -1,23 +1,10 @@
-use std::{
-    fs::File,
-    io::{prelude::*, BufReader},
-    path::Path,
-};
+use std::fs::File;
+use std::io::prelude::*;
 
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("unable to read file");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|s| s.expect("could not parse line"))
-        .collect()
-}
-
-fn main() {
-    // let data = fs::read_to_string("../../fortunes/fortunes").expect("unable to read file");
-    // let data: Vec<String> = data.split("%").map(|s| s.to_string()).collect();
-    // dbg!(&data);
-    let lines = lines_from_file("../../fortunes/fortunes");
-    for line in lines {
-        println!("{:?}", line);
-    }
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("../../fortunes/fortunes")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    dbg!(contents);
+    Ok(())
 }
